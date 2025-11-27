d47df4250ebe15686515ff62c6b0f6238354c2a4
infamousren <karenmgarcia404@gmail.com>
2025-11-27 20:50:09 +0800

    Done with this file for simple exercise

--- tests/Aut Exercise/codegen.spec.js
+++ tests/Aut Exercise/codegen.spec.js
@@ -3,19 +3,18 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
   await page.getByTestId('royal-email').click();
-  await page.getByTestId('royal-email').fill('--');
+  await page.getByTestId('royal-email').fill('insert email/username');
   await page.getByTestId('royal-pass').click();
-  await page.getByTestId('royal-pass').press('CapsLock');
-  await page.getByTestId('royal-pass').fill('F');
-  await page.getByTestId('royal-pass').press('CapsLock');
-  await page.getByTestId('royal-pass').fill('--');
+  await page.getByTestId('royal-pass').fill('insert pw');
   await page.getByTestId('royal-login-button').click();
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
-  await page.getByRole('textbox', { name: 'Password' }).fill('V');
-  await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
-  await page.getByRole('textbox', { name: 'Password' }).fill('--');
-  await page.getByRole('textbox', { name: 'Password' }).press('Enter');
-  await page.getByRole('button', { name: 'Log In' }).click();
+//need to duplicate test because of Facebook captcha
+  await page.getByTestId('royal-email').click();
+  await page.getByTestId('royal-email').fill('insert email/username');
+  await page.getByTestId('royal-pass').click();
+  await page.getByTestId('royal-pass').fill('insert pw');
+  await page.getByTestId('royal-login-button').click();
+
+ await page.goto('insert your profile link');
+ await expect(page).toHaveURL('profile link');
+
 });
